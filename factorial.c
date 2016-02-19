65636837dff861bcda167f138f4bd6ddbd090fb7
NigistaSaba <nigistasaba@gmail.com>
2016-02-20 01:10:22 +0300

    homework factorial

---
+++ factorial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+long factorial(int n)
+{
+	int c, result;
+	result = 1;
+	for (c = 1; c <= n; c++)
+		result = result * c;
+ 	return result;
+}
+int main ()
+{
+	int n;
+	long fact = 1;
+	printf("Input the number to calculate it's factorial > 0 \n");
+	scanf ("%d", &n);
+	printf("Factorial %d! = %d \n", n, factorial(n));
+	return 0;
+
+}	
---
+++ factorial_rec.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return (n * factorial (n - 1));
+}
+int main ()
+{
+	int n;
+	int fact = 1;
+	printf("Input the number to calculate it's factorial > 0 \n");
+	scanf ("%d", &n);
+	if (n < 0)
+		printf ("Input the number >0 \n");
+	else
+	{	
+		fact = factorial(n);
+		printf("Factorial %d! = %d \n", n, fact);
+	}
+return 0;
+}	
